85a264a08481ba146a9f354955aa2562bd970da7
Yves Navarro <yvesnf@gmail.com>
2015-09-28 20:22:17 +0200

    Programa amb classes

---
+++ yvesnavarro/helloworld_bootstrap.class.php
@@ -0,0 +1,18 @@
+<?php
+
+class Hello
+{	
+	
+	public $name = "Pepe";
+	
+	public function greetings($name = null){
+		if($name != null){
+			echo "Hola $name";
+		}else{
+			echo "Hola $this->name";
+		}
+		
+	}
+	
+}
+
