970b91740e8ed74cfcf9d8491db1029357b5bf68
Germangalia <germangalia@iesebre.com>
2015-09-28 19:39:24 +0200

    Modificació faker per a incloure HTML

--- German/faker.php
+++ German/faker.php
@@ -1,9 +1,36 @@
 <?php
 
-require "vendor/fzaninotto/faker/src/autoload.php"
+require "vendor/fzaninotto/faker/src/autoload.php";
 
-$faker = Faker\Factory::create();
+$fake = Faker\Factory::create();
 
-$name = $faker->name;
+$name = $fake->name;
 
-echo "hello $name\n"; 
+//echo "hello $name\n"; 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+   
+
+  </head>
+  <body>
+    <h1>Hello, <?php echo "hello $name\n"; ?>!</h1>
+
+  </body>
+</html>
+
