9af7a4dadce3396cf2d8b888b8af37373f4bfbb5
rogermelich <rogermelich@iesebre.com>
2015-09-28 20:56:46 +0200

    On hi es el Faker (On el Busca "Require")

--- roger/helloworldbootstrap.php
+++ roger/helloworldbootstrap.php
@@ -3,8 +3,5 @@ require "vendor/fzaninotto/faker/src/autoload.php";
 
 require "helloclass.php";
 
-$hello = new Hello();
-
-$faker = Faker\Factory::create();
-$name = $faker->name;
-//echo "hello $name \n";
+$faker = Faker\Factory::create(); 
+$hello = new Hello($faker);
