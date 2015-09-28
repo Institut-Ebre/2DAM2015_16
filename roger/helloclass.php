b58b5ddd8d7bca48334603504362cbffd1834f64
rogermelich <rogermelich@iesebre.com>
2015-09-28 20:56:03 +0200

    Arxiu Conte les Classes

--- roger/helloclass.php
+++ roger/helloclass.php
@@ -5,11 +5,20 @@ class Hello
 {
 	public $name = "Pepe";
 	
+	public $faker;
+	
+	public function __construct($faker) {
+		$this->faker = Faker\Factory::create();
+	}
+
     public function greetings($name = null) {
 		if ($name !=null){
 			echo "hello $name";
 		}else {
-		 echo "$hola $this->name";
+		 echo "Hola $this->name";
 		}
     }
+	public function greetings_random() {
+		echo "Hola" . $this->faker->name;
+	}    
 }
