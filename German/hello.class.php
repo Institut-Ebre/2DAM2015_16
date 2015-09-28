65a4e21c12a7e446e540963f8049a9471ab96261
Germangalia <germangalia@iesebre.com>
2015-09-28 20:25:17 +0200

    Afegit hello.class.php

---
+++ German/hello.class.php
@@ -0,0 +1,13 @@
+<?php
+
+class Hello
+{	
+	public $name ='Pepe';
+	
+	public function greetings($name = null) {
+		if ($name != null){
+			echo "Hola $name";
+		}else {
+			echo "Hola $this->name";
+	}
+}
