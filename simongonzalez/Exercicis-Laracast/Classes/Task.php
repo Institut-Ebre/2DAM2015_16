59e8cc4ea3c19d236ea3b8ac19992ec4b6ed7a15
SimonGonzalezCepeda <simongonzalez@iesebre.com>
2015-10-05 14:34:13 +0200

    Creació de classes

--- simongonzalez/Exercicis-Laracast/Classes/Task.php
+++ simongonzalez/Exercicis-Laracast/Classes/Task.php
@@ -1,5 +1,23 @@
 <?php
 
 class Task {
-    
+    public $description;
+
+    public $completed = false;
+
+    public function __construct($description)
+    {
+        $this->description = $description;
+    }
+
+    public function completed()
+    {
+        $this->completed = true;
+    }
 }
+
+$task = new Task('Learn OOP', 'The teacher bla bla bla...');
+
+$task2 = new Task('Pick up groceries');
+
+var_dump($task->completed);
