9181d25713bc217b2c61cae007ba8193b0ef9bb4
SimonGonzalezCepeda <simongonzalez@iesebre.com>
2015-10-13 12:46:01 +0200

    Created example 2.

---
+++ simongonzalez/Exercicis-Laracast/Interfaces/repository.php
@@ -0,0 +1,19 @@
+<?php
+
+interface Repository{
+    public function save($data);
+}
+
+class MongoRepository {
+    public function save($data)
+    {
+
+    }
+}
+
+class FileRepository {
+    public function save($data)
+    {
+
+    }
+}
