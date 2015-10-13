054335af9dfabd6a294585b285dc8908d3b5d6aa
SimonGonzalezCepeda <simongonzalez@iesebre.com>
2015-10-13 21:18:54 +0200

    Created v2 of the example

--- simongonzalez/exemple_mvc/mvc.php
+++ simongonzalez/exemple_mvc/mvc.php
@@ -5,7 +5,7 @@ class Model
     public $string;
 
     public function __construct(){
-        $this->string = "MVC + PHP = Awesome!";
+        $this->string = "MVC + PHP = Awesome, click here!";
     }
 }
 
@@ -19,8 +19,8 @@ class View
         $this->model = $model;
     }
 
-    public function output(){
-        return "<p>" . $this->model->string . "</p>";
+    public function output() {
+        return '<p><a href="mvc.php?action=clicked">' . $this->model->string . "</a></p>";
     }
 }
 
@@ -28,12 +28,21 @@ class Controller
 {
     private $model;
 
-    public function __construct($model) {
+    public function __construct($model){
         $this->model = $model;
+    }
+
+    public function clicked() {
+        $this->model->string = "Updated Data, thanks to MVC and PHP!";
     }
 }
 
 $model = new Model();
 $controller = new Controller($model);
 $view = new View($controller, $model);
+
+if (isset($_GET['action']) && !empty($_GET['action'])) {
+    $controller->{$_GET['action']}();
+}
+
 echo $view->output();
